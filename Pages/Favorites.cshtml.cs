455889687382e79f653b2fcea31a5243d28aa5a5
Lev Sokolov <levsokolov1410@gmail.com>
2026-04-12 18:59:41 +0300

    Unnecessary tabs have been removed, the favorites catalog and the basket have been left, everything works

---
+++ Pages/Favorites.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ShoesStore.Pages
+{
+    public class FavoritesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
