80150dec0f88bf6de6e5a2934ab0c7dcd14e52c9
Egor <sigmaboss406@gmail.com>
2026-04-16 18:51:20 +0300

    Some cleans in code

--- Pages/Favorites.cshtml.cs
+++ Pages/Favorites.cshtml.cs
@@ -1,12 +1,8 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ShoesStore.Pages
 {
     public class FavoritesModel : PageModel
     {
-        public void OnGet()
-        {
-        }
     }
 }
