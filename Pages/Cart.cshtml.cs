d375c48a4d5eb301c735811f198bb48af2a1c77b
Egor <sigmaboss406@gmail.com>
2026-04-16 18:51:20 +0300

    Some cleans in code

--- Pages/Cart.cshtml.cs
+++ Pages/Cart.cshtml.cs
@@ -1,12 +1,8 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ShoesStore.Pages
 {
     public class CartModel : PageModel
     {
-        public void OnGet()
-        {
-        }
     }
 }
